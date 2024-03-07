111445afb2041a46d16a897f8eed900a80862406
Anurag Yadav <yadavanurag839480@gmail.com>
2024-03-07 21:30:04 +0530

    anurag : Commiting Eureak Server microservice

--- Aircraft/src/main/java/AirlineManager/Aircraft/AircraftApplication.java
+++ Aircraft/src/main/java/AirlineManager/Aircraft/AircraftApplication.java
@@ -2,8 +2,10 @@ package AirlineManager.Aircraft;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AircraftApplication {
 
 	public static void main(String[] args) {
